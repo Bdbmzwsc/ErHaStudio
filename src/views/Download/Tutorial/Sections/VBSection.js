98dcd4704e917e1ee75815b96fa26796c79f8d88
samzhangjy <samzhang951@outlook.com>
2021-08-09 10:42:31 +0800

    web: 添加VB教程下载

--- src/views/Download/Tutorial/Sections/VBSection.js
+++ src/views/Download/Tutorial/Sections/VBSection.js
@@ -12,6 +12,13 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
 
@@ -30,6 +37,11 @@ const useStylesAcc = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
 }));
+const useStylesTable = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 export default function VBSection() {
   const [expanded, setExpanded] = React.useState(false);
@@ -37,8 +49,39 @@ export default function VBSection() {
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+  function createData(title, platform, link, linkShow) {
+    return { title, platform, link, linkShow };
+  }
+
+  const rowsVB = [
+    createData(
+      "第一集",
+      "BiliBili",
+      "https://www.bilibili.com/video/BV1fJ411G7r9",
+      "BV1fJ411G7r9"
+    ),
+    createData(
+      "第二集",
+      "BiliBili",
+      "https://www.bilibili.com/video/BV1oJ41177Hj",
+      "BV1oJ41177Hj"
+    ),
+    createData(
+      "第三集",
+      "BiliBili",
+      "https://www.bilibili.com/video/BV1t7411j7v1",
+      "BV1t7411j7v1"
+    ),
+    createData(
+      "第四集",
+      "BiliBili",
+      "https://www.bilibili.com/video/BV1FE411g7Wo",
+      "BV1FE411g7Wo"
+    ),
+  ];
   const classes = useStyles();
   const classesAcc = useStylesAcc();
+  const classesTable = useStylesTable();
   return (
     <div className={classes.section}>
       <GridContainer justify="center">
@@ -54,85 +97,41 @@ export default function VBSection() {
               aria-controls="panel1bh-content"
               id="panel1bh-header"
             >
-              <Typography className={classesAcc.heading}>
-                General settings
-              </Typography>
+              <Typography className={classesAcc.heading}>VB.Net教程</Typography>
               <Typography className={classesAcc.secondaryHeading}>
-                I am an accordion
+                共四集
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
-                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
-                feugiat. Aliquam eget maximus est, id dignissim quam.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            expanded={expanded === "panel2"}
-            onChange={handleChange("panel2")}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2bh-content"
-              id="panel2bh-header"
-            >
-              <Typography className={classesAcc.heading}>Users</Typography>
-              <Typography className={classesAcc.secondaryHeading}>
-                You are currently not an owner
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Donec placerat, lectus sed mattis semper, neque lectus feugiat
-                lectus, varius pulvinar diam eros in elit. Pellentesque
-                convallis laoreet laoreet.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            expanded={expanded === "panel3"}
-            onChange={handleChange("panel3")}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel3bh-content"
-              id="panel3bh-header"
-            >
-              <Typography className={classesAcc.heading}>
-                Advanced settings
-              </Typography>
-              <Typography className={classesAcc.secondaryHeading}>
-                Filtering has been entirely disabled for whole web server
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
-                Integer sit amet egestas eros, vitae egestas augue. Duis vel
-                augue.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            expanded={expanded === "panel4"}
-            onChange={handleChange("panel4")}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel4bh-content"
-              id="panel4bh-header"
-            >
-              <Typography className={classesAcc.heading}>
-                Personal data
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
-                Integer sit amet egestas eros, vitae egestas augue. Duis vel
-                augue.
-              </Typography>
+              <TableContainer component={Paper}>
+                <Table className={classesTable.table} aria-label="simple table">
+                  <caption>
+                    此教程为网络搜集得来，如有侵权请联系管理员删除。
+                  </caption>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>名称</TableCell>
+                      <TableCell>平台</TableCell>
+                      <TableCell>链接</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rowsVB.map((row) => (
+                      <TableRow key={row.title}>
+                        <TableCell component="th" scope="row">
+                          {row.title}
+                        </TableCell>
+                        <TableCell>{row.platform}</TableCell>
+                        <TableCell>
+                          <a href={row.link} target="_blank" rel="noreferrer">
+                            {row.linkShow}
+                          </a>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </AccordionDetails>
           </Accordion>
           <br />
