0e7761d0c35f18160fb4077b893162d8f3c2aa56
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2021-08-15 19:33:34 +0800

    Update index.js
    改了一点点东东

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import LandingPage from "views/LandingPage/LandingPage.js";
 import DownloadTutorial from "views/Download/Tutorial/Tutorial.js";
 import DownloadProject from "views/Download/Project/Project.js";
 import Msg from "views/Msg/Msg.js";
+import Login from "views/"
 import ScrollToTop from "views/ScrollToTop";
 import "@fontsource/roboto";
 import "@fortawesome/fontawesome-free/css/all.css";
@@ -24,6 +25,8 @@ ReactDOM.render(
       <Route path="/download/tutorial" component={DownloadTutorial} />
       <Route path="/download/project" component={DownloadProject} />
       <Route path="/msg" component={Msg} />
+      <Route path="/" component={LandingPage} />
+       <Route path="/msg" component={Msg} />
       <Route path="/" component={LandingPage} />
     </Switch>
   </Router>,
