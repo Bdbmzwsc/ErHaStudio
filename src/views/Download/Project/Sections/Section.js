131b53ce2da06d82e20665c1480e273b18b199f6
samzhangjy <samzhang951@outlook.com>
2021-10-10 19:34:22 +0800

    fix

--- src/views/Download/Project/Sections/Section.js
+++ src/views/Download/Project/Sections/Section.js
@@ -70,13 +70,7 @@ export default function Section() {
       "N/A",
       1
     ),
-     createData(
-      "SJAWeb",
-      "Web",
-      "https://bdbmzwsc.github.io/SJA",
-      "N/A",
-      2
-    ),
+    createData("SJAWeb", "Web", "https://bdbmzwsc.github.io/SJA", "N/A", 2),
   ];
   const rowsNitro = [
     createData(
