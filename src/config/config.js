581d7072a6bc1b897355335cd1d3fa2ec8de7e6e
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2022-07-19 11:27:59 +0800

    refactor(TeamSection): refactor

---
+++ src/config/config.js
@@ -0,0 +1,35 @@
+export const Config = [
+  {
+    name: "Bdbmzwsc",
+    img: "https://avatars.githubusercontent.com/u/88311279?s=400&u=9f913b319c2cf39977f8ca5e54d07052ca9d99c0&v=4",
+    identity: "室长、管理员",
+    des: "主要创始人",
+    qq_url:
+      "tencent://Message/?Uin=1726016497&websiteName=www.oicqzone.com&Menu=yes",
+    mail_url: "mailto:1726016497@qq.com",
+    gh_url: "https://github.com/Bdbmzwsc",
+    zhihu_url: "https://bdbmzwsc.github.io/MyBdbmzwsc",
+  },
+  {
+    name: "samzhangjy",
+    img: "https://avatars.githubusercontent.com/u/56634385?v=4",
+    identity: "管理员",
+    des: "初中生。他为二哈工作室编写了此网站，后者受到了高度评价。",
+    qq_url:
+      "tencent://Message/?Uin=2947602769&websiteName=www.oicqzone.com&Menu=yes",
+    mail_url: "mailto:samzhangjy@outlook.com",
+    gh_url: "https://github.com/samzhangjy",
+    zhihu_url: "https://www.zhihu.com/people/samzhangjy",
+  },
+  {
+    name: "还是samzhangjy?",
+    img: "https://avatars.githubusercontent.com/u/56634385?v=4",
+    identity: "管理员?",
+    des: "是个大美女",
+    qq_url:
+      "tencent://Message/?Uin=2947602769&websiteName=www.oicqzone.com&Menu=yes",
+    mail_url: "mailto:samzhangjy@outlook.com",
+    gh_url: "https://github.com/samzhangjy",
+    zhihu_url: "https://www.zhihu.com/people/samzhangjy",
+  },
+];
--- src/views/LandingPage/Sections/TeamSection.js
+++ src/views/LandingPage/Sections/TeamSection.js
@@ -15,10 +15,14 @@ import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
+//import { Box } from "@material-ui/core";
 
-import team1 from "assets/img/faces/bdb.jpg";
-import team2 from "assets/img/faces/buge.jpg";
-import team3 from "assets/img/faces/samzhangjy.jpg";
+//import team1 from "assets/img/faces/bdb.jpg";
+//import team2 from "assets/img/faces/buge.jpg";
+//import team3 from "assets/img/faces/samzhangjy.jpg";
+import { Config } from "../../../config/config";
+import { Box } from "@material-ui/core";
+//import { object } from "prop-types";
 
 const useStyles = makeStyles(styles);
 
@@ -34,6 +38,76 @@ export default function TeamSection() {
       <h2 className={classes.title}>这是我们的成员</h2>
       <div>
         <GridContainer>
+          {Config.map((configs, index) => {
+            return (
+              <GridItem xs={12} sm={12} md={4} key={index}>
+                <Card plain>
+                  <GridItem xs={14} sm={12} md={6} className={classes.itemGrid}>
+                    <Box sx={{ weight: 1000 }}>
+                      <img
+                        src={configs.img}
+                        alt="..."
+                        className={imageClasses}
+                      />
+                    </Box>
+                  </GridItem>
+                  <h4 className={classes.cardTitle}>
+                    {configs.name}
+                    <br />
+                    <small className={classes.smallTitle}>
+                      {configs.identity}
+                    </small>
+                  </h4>
+                  <CardBody>
+                    <p className={classes.description}>{configs.des}</p>
+                  </CardBody>
+                  <CardFooter className={classes.justifyCenter}>
+                    <Button
+                      justIcon
+                      color="transparent"
+                      className={classes.margin5}
+                      href={configs.qq_url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i className={classes.socials + " fab fa-qq"} />
+                    </Button>
+                    <Button
+                      justIcon
+                      color="transparent"
+                      className={classes.margin5}
+                      href={configs.mail_url}
+                    >
+                      <i className={classes.socials + " fas fa-envelope"} />
+                    </Button>
+                    <Button
+                      justIcon
+                      color="transparent"
+                      className={classes.margin5}
+                      href={configs.gh_url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i className={classes.socials + " fab fa-github"} />
+                    </Button>
+                    <Button
+                      justIcon
+                      color="transparent"
+                      className={classes.margin5}
+                      href={configs.zhuhu_url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i className={classes.socials + " fab fa-zhihu"} />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </GridItem>
+            );
+          })}
+          {/*
+
+          
           <GridItem xs={12} sm={12} md={4}>
             <Card plain>
               <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
@@ -194,7 +268,7 @@ export default function TeamSection() {
                 </Button>
               </CardFooter>
             </Card>
-          </GridItem>
+          </GridItem>*/}
         </GridContainer>
       </div>
     </div>
